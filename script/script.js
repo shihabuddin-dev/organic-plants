954ac2fdbc32a691251f801916137b2b561edfe4
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-03-12 19:58:33 +0600

    i have added here dynamic contents

---
+++ script/script.js
@@ -0,0 +1,10 @@
+document
+  .getElementById("product-container")
+  .addEventListener("click", function (e) {
+    if (e.target.tagName == "BUTTON") {
+      const button = e.target;
+      // console.log(
+      //   button.parentNode.parentNode.children[1].children[1].innerText
+      // );
+    }
+  });
